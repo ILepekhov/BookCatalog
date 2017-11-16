4c74282074bcab27a14a85d9f0c498cc3769f9e2
Иван Лепехов <lepvanek@gmail.com>
2017-11-16 09:13:48 +0500

    Добавлено решение

---
+++ Shared/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    [Serializable]
+    public sealed class Book
+    {
+        #region Properties
+
+        public string Authors { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public uint PublicationYear { get; set; }
+        public byte[] Image { get; set; }
+        public string EBookURL { get; set; }
+        public CatalogSection Section { get; set; }
+
+        #endregion
+
+        #region .ctor
+
+        public Book(string title, string authors, uint publicationYear, CatalogSection section)
+        {
+            Title = title;
+            Authors = authors;
+            PublicationYear = publicationYear;
+            Section = section;
+        }
+
+        #endregion
+    }
+}
